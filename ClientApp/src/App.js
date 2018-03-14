7b2f9d56837c2661230bbb4213acd764b85705b5
Michael Lawrence <mlawrence@gatesmandave.com>
2018-03-14 15:43:22 -0400

    added code splitting with react loadable

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,11 +1,36 @@
 ﻿import React from 'react';
 import { Route } from 'react-router';
+import Loadable from 'react-loadable';
+
+import RouteLoader from './components/RouteLoader';
 import Layout from './components/Layout';
-import Home from './components/Home';
-import Counter from './components/Counter';
-import FetchData from './components/FetchData';
-import TeyCalc from './components/TeyCalc'
-import Quiz from './components/Quiz'
+// import Home from './components/Home';
+// import Counter from './components/Counter';
+// import FetchData from './components/FetchData';
+// import TeyCalc from './components/TeyCalc'
+// import Quiz from './components/Quiz'
+
+const Home = Loadable({
+  loader: () => import('./components/Home'),
+  loading: RouteLoader,
+});
+const Counter = Loadable({
+  loader: () => import('./components/Counter'),
+  loading: RouteLoader,
+});
+const FetchData = Loadable({
+  loader: () => import('./components/FetchData'),
+  loading: RouteLoader,
+});
+const TeyCalc = Loadable({
+  loader: () => import('./components/TeyCalc'),
+  loading: RouteLoader,
+});
+const Quiz = Loadable({
+  loader: () => import('./components/Quiz'),
+  loading: RouteLoader,
+});
+
 
 export default () => (
   <Layout>
---
+++ ClientApp/src/components/RouteLoader.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import LoaderImage from '../assets/30.gif';
+import './RouteLoader.css';
+
+
+export default props => (
+    <div className="loader"><img src={LoaderImage} alt="loading..."/></div>
+)
